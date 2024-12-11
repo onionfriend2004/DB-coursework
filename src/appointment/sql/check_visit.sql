f9496e0cc05b7a95dc5440b03b52533b41a0979d
Alexey Lupenkov <alupenkov@inbox.ru>
2024-12-11 16:18:02 +0300

    fix

--- src/appointment/sql/check_visit.sql
+++
@@ -1,6 +0,0 @@
-SELECT 1
-FROM visits
-WHERE doctor_id = '$doctor'
-    AND appointment_date = '$appointment_date'
-    AND appointment_time_start = '$appointment_time_start'
-    AND appointment_time_end = '$appointment_time_end'
